51db4df2bd7fc8672f42efdaaff926a11c7397da
DefiantTaco <mackan_lindback@hotmail.com>
2019-02-06 18:25:57 +0100

    MainSpel

---
+++ Mainspel/Assets/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+    
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    
+
+    // Update is called once per frame
+    void Update () {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            animator.SetBool("IsAttacking", true);
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            animator.SetBool("IsAttacking", false);
+        }
+
+
+        if (Input.GetKeyDown("down"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetKeyUp("down"))
+        {
+            crouch = false;
+        }
+
+	}
+
+    public void OnLanding ()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
