d393673c5cc72b2360a32e93eadf149b77eccb65
DefiantTaco <45100576+DefiantTaco@mackan_lindback@hotmail.com>
2019-02-28 19:58:41 +0100

    TEMP

---
+++ PacMan/New Unity Project/Assets/Scripts/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+	public bool isPortal;
+
+	public bool isPellet;
+	public bool isSuperPellet;
+
+	public bool didConsumePlayerOne;
+	public bool didConsumePlayerTwo;
+
+	public bool isGhostHouseEntrance;
+	public bool isGhostHouse;
+
+	public bool isBonusItem;
+	public int pointValue;
+
+	public GameObject portalReceiver;
+}
